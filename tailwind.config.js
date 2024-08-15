bbfaa6560ab50e6dcb004f4b48a2415580fdeb78
itsreee <sreelakshmismenon8086@gmail.com>
2024-08-15 05:46:57 +0530

    fabrica

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+       rubik: ["Rubik","sans-serif"],
+       gupter:["Grey-go","cursive"],
+       edubeginner:["Edu VIC WA NT Beginner","cursive"],
+      },
+    },
+  },
+  plugins: [],
+}
+
